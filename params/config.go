286ebcdafc8a280e1b2f7349083e92265e991772
ilhandahir1122-debug <ilhandahir1122@gmail.com>
2025-11-30 21:51:10 +0300

    Update config.go

--- params/config.go
+++ params/config.go
@@ -424,7 +424,22 @@ var NetworkNames = map[string]string{
 	HoleskyChainConfig.ChainID.String(): "holesky",
 	HoodiChainConfig.ChainID.String():   "hoodi",
 }
-
+var LuwaChainConfig = &ChainConfig{
+    ChainID:             big.NewInt(9882),
+    HomesteadBlock:      big.NewInt(0),
+    EIP150Block:         big.NewInt(0),
+    EIP155Block:         big.NewInt(0),
+    EIP158Block:         big.NewInt(0),
+    ByzantiumBlock:      big.NewInt(0),
+    ConstantinopleBlock: big.NewInt(0),
+    PetersburgBlock:     big.NewInt(0),
+    IstanbulBlock:       big.NewInt(0),
+    MuirGlacierBlock:    big.NewInt(0),
+    BerlinBlock:         big.NewInt(0),
+    LondonBlock:         big.NewInt(0),
+    ArrowGlacierBlock:   big.NewInt(0),
+    GrayGlacierBlock:    big.NewInt(0),
+}
 // ChainConfig is the core config which determines the blockchain settings.
 //
 // ChainConfig is stored in the database on a per block basis. This means
